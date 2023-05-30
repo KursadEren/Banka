2937f7718ebdfbb3e298eec2aac5c9b1bacba673
KursadEren <k.erenmaden1@outlook.com>
2023-05-30 12:46:39 +0300

    App.js Update native stack Context

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StyleSheet   } from 'react-native';
+import SignIn from './src/Screen/SignIn';
+import SignUp from './src/Screen/SignUp';
+import  { MyContextProvider } from './src/Context/Context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import DraverNavigator from './src/Screen/HomeScreen';
+import Hesap from './src/Screen/Hesap';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const BetweenNavigator = () =>{
+ 
+  
+  const transitionAnimation = (props) => {
+    const { scene } = props;
+    const { position, layout, scene: { progress } } = scene;
+    const width = layout.initWidth;
+    const translateX = position.interpolate({
+      inputRange: [scene.index - 1, scene.index, scene.index + 1],
+      outputRange: [-width, 0, width],
+    });
+    const opacity = progress.interpolate({
+      inputRange: [0, 1, 2],
+      outputRange: [0, 1, 0],
+    });
+    return {
+      transform: [{ translateX }],
+      opacity,
+    };
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     
+      
+      <Stack.Navigator initialRouteName="SignIn" screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: 'white' },
+        cardStyleInterpolator: transitionAnimation,
+      }}>
+        <Stack.Screen name="Sign In" component={SignIn} />
+        <Stack.Screen name="DraverNavigator" component={DraverNavigator} />
+        <Stack.Screen name="Sign Up" component={SignUp} />
+        <Stack.Screen name="Hesap" component={Hesap} />
+
+      </Stack.Navigator>
+      
+    </NavigationContainer>)
+ }
+export default function App() {
+ 
+  
+  return (
+    <MyContextProvider >
+      
+          <BetweenNavigator/>
+       
+    </MyContextProvider>
+    
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
+  appBar:{
+    marginTop:'10.5%'
+  }
 });
